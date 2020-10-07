caf357ecf2d32284860fd3a6eb2c69865dcba47a
Mike Boring <shekinah999@msn.com>
2020-10-06 22:10:05 -0600

    Login, Logout and Signup working

--- microbrewforyou_app/views.py
+++ microbrewforyou_app/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect, reverse
+from django.contrib.auth import login, logout, authenticate
 
+from microbrewforyou_app.models import CustomUser
+from microbrewforyou_app.forms import LoginForm, SignupForm
 # Create your views here.
+
+
+def index(request):
+    return render(request, "index.html")
+
+
+def login_view(request):
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            user = authenticate(request, username=data.get(
+                "username"), password=data.get("password"))
+            if user:
+                login(request, user)
+                return HttpResponseRedirect(request.GET.get('next', reverse("homepage")))
+    form = LoginForm()
+    return render(request, "generic_form.html", {"form": form})
+
+
+def signup_view(request):
+    if request.method == "POST":
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            new_user = CustomUser.objects.create_user(
+                username=data.get("username"), password=data.get(
+                    "password"), first_name=data.get(
+                        "first_name"), bio=data.get(
+                            "bio"), user_image=data.get(
+                                "user_image"), address=data.get(
+                                    "address"), city=data.get(
+                                        "city"), state=data.get("state"))
+            login(request, new_user)
+            return HttpResponseRedirect(reverse("homepage"))
+
+    form = SignupForm()
+    return render(request, "generic_form.html", {"form": form})
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("homepage"))
--- microbrewforyou_server/urls.py
+++ microbrewforyou_server/urls.py
@@ -14,10 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include 
-from microbrewforyou_app import views 
+from django.urls import path, include
+
+from microbrewforyou_app.views import index, login_view, logout_view, signup_view
 
 
 urlpatterns = [
+    path('', index, name="homepage"),
+    path('login/', login_view, name="loginview"),
+    path('signup/', signup_view, name="signupview"),
+    path('logout/', logout_view, name="logoutview"),
     path('admin/', admin.site.urls),
 ]
