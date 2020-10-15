7522895f7ac8093ec67c0ca72bc210d45330db15
Mike Boring <shekinah999@msn.com>
2020-10-15 10:15:59 -0600

    Added model form for signup for rubric item

--- microbrewforyou_app/forms.py
+++ microbrewforyou_app/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from microbrewforyou_app.models import CustomUser
 
 
 class LoginForm(forms.Form):
@@ -6,14 +7,18 @@ class LoginForm(forms.Form):
     password = forms.CharField(widget=forms.PasswordInput)
 
 
-class SignupForm(forms.Form):
-    username = forms.CharField(max_length=240)
-    password = forms.CharField(widget=forms.PasswordInput)
-    first_name = forms.CharField(max_length=240)
-    bio = forms.CharField(max_length=280)
-    address = forms.CharField(max_length=280)
-    city = forms.CharField(max_length=50)
-    state = forms.CharField(max_length=50)
+class SignupForm(forms.ModelForm):
+    class Meta:
+        model = CustomUser
+        fields = ['username', 'password', 'first_name',
+                  'bio', 'address', 'city', 'state']
+    # username = forms.CharField(max_length=240)
+    # password = forms.CharField(widget=forms.PasswordInput)
+    # first_name = forms.CharField(max_length=240)
+    # bio = forms.CharField(max_length=280)
+    # address = forms.CharField(max_length=280)
+    # city = forms.CharField(max_length=50)
+    # state = forms.CharField(max_length=50)
 
 
 class EditUserForm(forms.Form):
