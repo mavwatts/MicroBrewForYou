14954acb5d30dfb20584a8e4c68520f3542b03ba
Mike Boring <shekinah999@msn.com>
2020-10-16 14:53:48 -0600

    fixing errors

--- microbrewforyou_app/forms.py
+++ microbrewforyou_app/forms.py
@@ -14,14 +14,11 @@ class SignupForm(forms.ModelForm):
                   'bio', 'address', 'city', 'state']
 
 
-class EditUserForm(forms.Form):
-    username = forms.CharField(max_length=240)
-    first_name = forms.CharField(max_length=240)
-    bio = forms.CharField(max_length=280)
-    address = forms.CharField(max_length=280)
-    city = forms.CharField(max_length=50)
-    state = forms.CharField(max_length=50)
-    user_image = forms.ImageField(required=False)
+class EditUserForm(forms.ModelForm):
+    class Meta:
+        model = CustomUser
+        fields = ['username', 'first_name', 'user_image',
+                  'bio', 'address', 'city', 'state']
 
 
 class PostForm(forms.Form):
